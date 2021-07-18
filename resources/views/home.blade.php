532f12a5460aee430e4199c4ccbf711115de1609
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-07-18 13:22:17 +0530

    Installed laravel via composer and remove public from url

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Home Page</title>
+	<link href="{{asset('public/bs5/bootstrap.min.css')}}" rel="stylesheet" />
+</head>
+<body>
+	<h1>Home Page</h1>
+</body>
+</html>
