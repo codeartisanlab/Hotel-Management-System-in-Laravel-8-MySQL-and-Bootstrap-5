bd7ec4fa9476bf999d2e1d56a18649877f24426b
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-07-21 23:32:36 +0530

    part-4-configure-admin-template-and-create-base-template

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return view('home');
 });
+
+// Admin Dashboard
+Route::get('admin', function(){
+    return view('dashboard');
+});
