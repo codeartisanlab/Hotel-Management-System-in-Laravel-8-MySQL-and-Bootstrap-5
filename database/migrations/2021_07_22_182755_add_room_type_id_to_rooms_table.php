b0ef1f89e6e7b1703e411d3a8f640c77bc0433fd
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-07-23 00:01:28 +0530

    part-5-create-room-types-with-rooms model and migrations

---
+++ database/migrations/2021_07_22_182755_add_room_type_id_to_rooms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoomTypeIdToRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->integer('room_type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            //
+        });
+    }
+}
