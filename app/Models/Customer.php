d3a2a83a0946c136261c9c7c17c1867047ee488f
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-08-31 01:12:45 +0530

    part-17-booking-system-create-model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    function bookings(){
+        return $this->hasMany(Booking::class);
+    }
 }
---
+++ database/migrations/2021_08_30_192906_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->integer('room_id');
+            $table->string('checkin_date');
+            $table->string('checkout_date');
+            $table->string('total_adults');
+            $table->string('total_children');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
